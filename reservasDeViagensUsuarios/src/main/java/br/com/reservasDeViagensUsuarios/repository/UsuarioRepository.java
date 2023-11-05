2a2409b570a7a6eb215131d55040721180e42665
Thiago Tobias <thiagotobias@gmail.com>
2023-11-05 20:59:15 -0300

    removendo o user

--- reservasDeViagensUsuarios/src/main/java/br/com/reservasDeViagensUsuarios/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.reservasDeViagensUsuarios.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.reservasDeViagensUsuarios.model.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
